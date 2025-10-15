5f8b7776909d15b8e56bd690b433a24c6ff6e9aa
soxibdavurov <soxib.davurov@gmail.com>
2025-10-16 00:53:22 +0900

    feat: publish basicLayout component

--- libs/components/layout/LayoutBasic.tsx
+++ libs/components/layout/LayoutBasic.tsx
@@ -1,5 +1,7 @@
 import { Stack } from "@mui/material"
 import Head from "next/head"
+import Footer from "../Footer";
+import Top from "../Top";
 
 const withLayoutBasic = (Component: any) => {
     return (props: any) => {
@@ -9,12 +11,31 @@ const withLayoutBasic = (Component: any) => {
                     <title>Nestar</title>
                 </Head>
                 <Stack id="pc-wrap">
-                    <Stack >Header Basic</Stack>
+                    <Stack id={"top"}>
+                        <Top />
+                    </Stack>
+
+                    <Stack className={"header-basic"}
+                        style={{
+                            backgroundImage: `url(img/banner/properties.png)`,
+                            backgroundSize: "cover",
+                            boxShadow: "inset 10px 40px 150px 40px rgb(24 22 36)",
+                        }}>
+
+                        <Stack className={"container"}>
+                            <strong>Property Search</strong>
+                            <span>We are so glad to feel you here again!</span>
+                        </Stack>
+                    </Stack>
+
+
                     <Stack id={"main"}>
                         <Component {...props} />
                     </Stack>
 
-                    <Stack>Footer</Stack>
+                    <Stack id={"footer"}>
+                        <Footer />
+                    </Stack>
                 </Stack>
             </>
         );
