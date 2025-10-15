8cd2bcb6876b7721dd488860dc52151ea7e0a201
soxibdavurov <soxib.davurov@gmail.com>
2025-10-16 01:15:56 +0900

    fix: refactor pages components

--- libs/components/layout/LayoutBasic.tsx
+++ libs/components/layout/LayoutBasic.tsx
@@ -23,7 +23,7 @@ const withLayoutBasic = (Component: any) => {
                         }}>
 
                         <Stack className={"container"}>
-                            <strong>Property Search</strong>
+                            <strong>Search</strong>
                             <span>We are so glad to feel you here again!</span>
                         </Stack>
                     </Stack>
--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,16 +1,13 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { Container, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import { NextPage } from "next";
 
 const PropertyList: NextPage = () => {
-
-    console.log("PROPERTYLIST COMPONENT - PAGES ROUTER");
     return (
-        <>
-            <Container> PROPERTY LIST</Container>
-
-        </>
-    )
-}
+        <div style={{ margin: "20px 0" }}>
+            <Stack className={"container"}>PROPERTY LIST</Stack>
+        </div>
+    );
+};
 
 export default withLayoutBasic(PropertyList);
